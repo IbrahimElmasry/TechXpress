4d5de66b79fc327017307989bf7bf9ecbdaf09c0
Ibrahim Tarek <120456709+IbrahimElmasry@users.noreply.github.com>
2024-10-10 20:19:37 +0300

    Adding DashBoard Main View and Controller

---
+++ TechXpress.Web/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TechXpress.Entities.Repositories;
+using TechXpress.Utilities;
+
+namespace TechXpress.Web.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+    [Authorize(Roles ="Admin")]
+    public class DashboardController : Controller
+	{
+
+		private IUnitOfWork _unitOfWork;
+		public DashboardController(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+
+		public IActionResult Index()
+		{
+			ViewBag.Orders = _unitOfWork.OrderHeader.GetAll().Count();
+			ViewBag.ApprovedOrders = _unitOfWork.OrderHeader.GetAll(x => x.OrderStatus == SD.Approve).Count();
+			ViewBag.Users = _unitOfWork.ApplicationUser.GetAll().Count();
+			ViewBag.Products = _unitOfWork.Product.GetAll().Count();
+			return View();
+		}
+	}
+}
--- TechXpress.Web/Program.cs
+++ TechXpress.Web/Program.cs
@@ -7,41 +7,47 @@ using TechXpress.Entities.Repositories;
 using TechXpress.Utilities;
 using TexhXpress.DataAccess.Implementation;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
-builder.Services.AddDbContext<ApplicationDbContext>(options=>options.UseSqlServer(
-builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 builder.Services.Configure<StripeData>(builder.Configuration.GetSection("stripe"));
 
-builder.Services.AddIdentity<IdentityUser , IdentityRole>(options=>options.Lockout.DefaultLockoutTimeSpan= TimeSpan.FromDays(1))
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromDays(1))
     .AddDefaultTokenProviders().AddDefaultUI()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
-
 builder.Services.AddSingleton<IEmailSender, EmailSender>();
-builder.Services.AddScoped<IUnitOfWork , UnitOfWork>();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+// Define Authorization Policy for Admin role
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("Admin", policy => policy.RequireRole("Admin"));
+});
 
 var app = builder.Build();
-
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 StripeConfiguration.ApiKey = builder.Configuration.GetSection("stripe:Secretkey").Get<string>();
+
+// Make sure to include authentication middleware
+app.UseAuthentication(); // Ensure this line is added before authorization
 
 app.UseAuthorization();
 app.MapRazorPages();
